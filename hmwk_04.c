2472c9c53a06387e7878351c78367c2eb423fe05
Namrata Sivakumar <43491187+namratasiv@users.noreply.github.com>
2019-05-21 19:49:35 -0500

    Add files via upload

---
+++ hmwk_04.c
@@ -0,0 +1,105 @@
+// Sivakumar, Namrata
+// nxs8168
+// 2019-03-07
+
+#include <ctype.h>
+#include <regex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *getToken( FILE *fp );
+int status1, status2, status3;
+int fp, str, intt;
+regex_t re1, re2, re3;
+
+const char* fppattern = "^[0-9]+[)]{1}[(]{1}[0-9]+$";
+const char* intpattern = "^[#]{1}[r-wR-W0-9]+$";
+const char* strpattern = "^[|]{1}[A-Z0-9]*[+]{1}$";
+
+
+void processToken( char *token )
+{
+  fp = regcomp(&re1, fppattern, REG_EXTENDED);
+  str = regcomp(&re2, strpattern, REG_EXTENDED);
+  intt = regcomp(&re3, intpattern, REG_EXTENDED);
+
+  int flag = 0;
+      if (fp == 0 ) {
+          status1 = regexec(&re1, token, 0, NULL, 0);
+          if (status1 == 0 && flag == 0){
+            printf(">%s< matches FP.\n", token);
+            flag = 1;
+          }
+      }
+    if (str == 0){
+      status3 = regexec(&re2, token, 0, NULL, 0);
+        if (status3 == 0 && flag == 0){
+          printf(">%s< matches STR.\n", token);
+          flag = 1;
+        }    
+    }
+     if (intt==0){
+      status2 = regexec(&re3, token, 0, NULL, 0);
+        if (status2 == 0 && flag == 0){
+          printf(">%s< matches INT.\n", token);
+          flag = 1;
+        }
+    }
+    if(flag == 0){
+        printf(">%s< does not match.\n", token);
+    }
+    regfree(&re1);
+    regfree(&re2);
+    regfree(&re3);
+}
+
+int main( int argc, char *argv[] )
+{
+  if ( argc > 1 ) {
+    printf( "processing tokens from %s ...\n", argv[1] );
+
+    FILE *fp = fopen( argv[1], "r" );
+    char *token = NULL;
+
+    if ( fp != NULL ) {
+      while ( token = getToken( fp ) ) {
+        processToken( token );
+        free( token );
+      }
+
+      fclose( fp );
+    } else {
+      printf( "unable to open %s?\n", argv[1] );
+    }
+  } else {
+    printf( "No input file specified.\n" );
+  }
+}
+
+#define MAX_TOKEN_SIZE (1024)
+
+char *getToken( FILE *fp )
+{
+  char token[ MAX_TOKEN_SIZE+1 ];
+  int  i  = 0;
+
+  while ( i < MAX_TOKEN_SIZE ) {
+    int ch = fgetc( fp );
+
+    if ( ch == EOF ) { break; }
+
+    if ( isspace( ch ) ) {
+      if ( i == 0 ) { continue; }
+      break;
+    }
+
+    token[ i++ ] = ch;
+  }
+
+  if ( i == 0 ) { return NULL; }
+
+  token[ i ] = '\0';
+
+  return strdup( token );
+}
---
+++ hmwk_04.cc
@@ -0,0 +1,53 @@
+// Sivakumar, Namrata
+// nxs8168
+// 2019-02-28
+
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+using namespace std;
+
+void processToken( string token )
+{
+  regex s("[|]{1}[A-Z0-9]*[+]{1}");
+  regex fp("[0-9]+[)]{1}[(]{1}[0-9]+");
+  regex intt("[#]{1}[r-wR-W0-9]+");
+  if(regex_match(token,s)){
+cout<<">"<<token<<"<"<<" matches STR."<<endl;
+  }
+  
+  else if(regex_match(token,fp)){
+cout<<">"<<token<<"<"<<" matches FP."<<endl;
+  }
+else if(regex_match(token,intt)){
+cout<<">"<<token<<"<"<<" matches INT."<<endl;
+  }
+  else{
+    cout<<">"<<token<<"<"<<" does not match."<<endl;
+  }
+  //cout << ">" << token << "< is the proposed token.\n";
+}
+
+int main( int argc, char *argv[] )
+{
+  if ( argc > 1 ) {
+    cout << "processing tokens from " << argv[ 1 ] << " ...\n";
+
+    ifstream inputFile;
+    string   token;
+
+    inputFile.open( argv[ 1 ] );
+
+    if ( inputFile.is_open() ) {
+      while ( inputFile >> token ) {
+        processToken( token );
+      }
+      inputFile.close();
+    } else {
+      cout << "unable to open " << argv[ 1 ] << "?\n";
+    }
+  } else {
+    cout << "No input file specified.\n";
+  }
+}
